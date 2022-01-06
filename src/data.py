5d8d81960e63bef7156917b20814a410de8ece54
hainguyentado <95960294+hainguyentado@users.noreply.github.com>
2022-01-06 17:22:36 +0700

    neg1

--- src/data.py
+++ src/data.py
@@ -186,7 +186,7 @@ class TaskGenerator(object):
             self.item_pool = set(self.ratings['itemId'].unique())
 
             # create negative item samples
-            self.negatives_train = self._sample_negative2(self.ratings)
+            self.negatives_train = self._sample_negative()
             self.train_ratings = self.ratings
         
     
@@ -207,7 +207,7 @@ class TaskGenerator(object):
         neg_samples.close()
         return negatives_train
 
-    def _sample_negative2(self, ratings): # sample ngẫu nhiên negative từ tập train
+    def _sample_negative2(self): # sample ngẫu nhiên negative từ tập train
         by_userid_group = self.ratings.groupby("userId")['itemId']
         negatives_train = {}
         for userid, group_frame in by_userid_group:
